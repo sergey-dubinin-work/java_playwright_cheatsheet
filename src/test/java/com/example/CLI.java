d33262b104ec7d153415e551cec4a9f6e2588e06
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-05-21 14:10:33 +0300

    CLI

---
+++ src/test/java/com/example/CLI.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class CLI {
+    String installPlaywright        = "npm playwright install";
+
+    String openPlaywrightInspector  = "npx playwright codegen";
+
+    String openTraceViewer          = "npx playwright show-trace archive.zip";
+}
